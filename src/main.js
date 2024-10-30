e36c2cc53b4dc64a5182cbeeeb193415541e4d03
Josh <joshsetterstrom@gmail.com>
2024-10-30 15:01:34 -0700

    20241030

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import './main.css';
 import logo from './assets/initek.png';
+import emailIco from './assets/email-ico.png';
+import phoneIco from './assets/phone-ico.png';
+import mapsIco from './assets/maps-ico.png';
 import canvas from './assets/home_canvas.jpg';
+import starFront from './assets/start_front.jpg';
 import { useLayoutEffect, useState } from 'react';
 
 export default function Main() {
 	const [navRedirect, setNavRedirect] = useState(false);
-	const [p1t, setp1t] = useState({opacity: 1, position: 'absolute'});
+	const [p1t, setp1t] = useState({opacity: 1, position: 'absolute', left: 0});
 	const [p1i, setp1i] = useState({marginTop: 0});
 	const [p2t, setp2t] = useState({opacity: 1, pointerEvents: 'none', left: '-30vw', transition: 'opacity 1s, left 1s'});
 	const [p2d, setp2d] = useState({opacity: 0, top: '20vh', transition: 'opacity 1s, top 1s ease-in-out'});
@@ -14,7 +18,7 @@ export default function Main() {
 
 	const [servicesPage, setServicesPage] = useState(0);
 
-	const sections = ['home', 'about us', 'services', 'contact'];
+	const sections = ['home', 'about us', 'our services', 'contact us'];
 
 
 
@@ -97,7 +101,7 @@ export default function Main() {
 			target='_blank'
 			onClick={() => {document.getElementById(anchor)?.scrollIntoView({behavior: 'smooth'})}}
 		>
-			{anchor}
+			{anchor.toUpperCase()}
 		</a>
 	));
 
@@ -172,76 +176,162 @@ export default function Main() {
 			<header>
 				<img src={logo} alt="logo"/>
 				<nav>
-					{anchors}	
+					{anchors}
 				</nav>
 			</header>
 
 			<main>
-				<section id='home'>
-					<div className='img-container' style={{width: '90vw', height: '70vh', backgroundColor: 'blue', borderRadius: 30, margin: '140px 10vw', overflow: 'hidden'}}>
+				<section id='home' style={{justifyContent: 'center', alignItems: 'center'}}>
+					<div className='img-container' style={{width: '80vw', height: '70vh', backgroundColor: 'black', borderRadius: 30, overflow: 'hidden'}}>
 						<div className='gradient top-light' style={{width: '80vw'}}/>
 						<div className='gradient bot' style={{width: '80vw'}}/>
 						<div className='gradient left'/>
-						<img id='page1-img' src={canvas} style={{...p1i, borderRadius: 30, width: '90vw', height: '90vh'}} alt="canvas"/>
+						<img id='page1-img' src={canvas} style={{...p1i, borderRadius: 30, width: 2000, height: '90vh'}} alt="canvas"/>
 					</div>
 					<div className='page-content left' style={p1t}>
 						<h2 id='title-logo'>INITEK SOLUTIONS</h2>
 						<p id='title-p'>Providing a wide range of voice, data, and networking solutions for businesses of all sizes, aiming to deliver cost-effective and efficient options to meet your communication and networking needs.</p>
-						<a
-							target='_blank'
+						<button
 							onClick={() => document.getElementById('contact')?.scrollIntoView({behavior: 'smooth'})}
 						>
 							Book a consultation
-						</a>
+						</button>
 					</div>
 				</section>
 
 				<section id='about us'>
-					<div style={{width: '90vw', margin: '140px 10vw'}}>
-						<p style={{fontSize: 48, fontWeight: 'bold'}}>
-							At Initek Solutions we provide innovative ideas to increase proficiency by catering to your business' operational demands. 
-							{/* Our technicians are knowledgeable and experienced with installation and troubleshooting the industry's leading technology and products. Providing efficient and effective voice and data solutions to your organization is crucial in today's society.  */}
-							<br/><br/>
-							{/* Contact our service professionals to discuss how Initek can provide better solutions for your organization.						 */}
-						</p>
-						<div style={{display: 'flex', flexDirection: 'row'}}>
-							<div style={{height: 400, width: 400, border: '1px solid #2f72d0', margin: 20}}>
-
-							</div>
-							<div style={{height: 400, width: 400, border: '1px solid #2f72d0', margin: 20}}>
-
-							</div>
-							<div style={{height: 400, width: 400, border: '1px solid #2f72d0', margin: 20}}>
-
-							</div>
+					<div style={{display: 'flex', width: '90vw', margin: '140px 10vw', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
+						<div style={{height: '90%', width: '50%', border: '1px solid #144283'}}>Image here</div>
+						<div style={{width: '50%', textAlign: 'end', marginLeft: 30}}>
+							<h2 style={{}}>We provide innovative ideas to increase proficiency by catering to your business' operational demands</h2>
+							<p style={{fontSize: 20}}>
+								Our technicians are knowledgeable and experienced with installation and troubleshooting the industry's leading technology and products. 
+								Providing efficient and effective voice and data solutions to your organization is crucial in today's society.
+							</p>
 						</div>
 					</div>
 				</section>
 
-				{/* <section id='services' style={{height: '150vh'}}>
-					<div className='page-content center-right' style={p3t}>
-						<div className='container-content'>
-							{servicePages[servicesPage]}
-							<div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}>
-								{bullets}
+				<section id='our services'>
+					<div style={{display: 'flex', width: '90vw', margin: '140px 11vw', justifyContent: 'center', backgroundColor: '#000812', borderRadius: 10}}>
+						<div className='item-box'>
+							<div style={{fontSize: 30}}>
+								Business Phone Systems
 							</div>
+							<br/>
+							<div style={{fontSize: 18}}>
+								Initek Solutions is diversified in troubleshooting, programming and installing any of the following systems, to include voicemail and mail systems.
+							</div>
+							<ul style={{fontSize: 16}}>
+								<li>Nortel BCM</li>
+								<li>Nortel Norstar</li>
+								<li>Meridian Option 11c</li>
+								<li>Nortel Call Pilot</li>
+								<li>Avaya IP Office</li>
+								<li>VoIP phone sets</li>
+								<li>Desk phones, cordless, conference phones and headsets</li>
+							</ul>
+							<div style={{fontSize: 18}}>
+								If you require assistance in choosing the best phone system to support your business, contact us at your earliest convenience.
+							</div>
+						</div>
+						<div className='item-box'>
+							<div style={{fontSize: 30}}>
+								Cable/Fiber Installation
+							</div>
+							<br/>
+							<div style={{fontSize: 18}}>
+								Initek Solutions provides installation, repair and testing of all the following:
+							</div>
+
+							<ul style={{fontSize: 16}}>
+								<li>
+								Voice/ Data runs: cat5e, cat6, cat6a or any voice/data cabling (FT6 or FT4 rated, plenum, indoor, or outdoor) as per regulations.
+								</li>
+								<li>
+								Fiber, small to large scale installation (multimode 50/125, 62.5/125, single mode, SC, LC, ST connectors, indoor or outdoor).
+								</li>
+								<li>
+								Coaxial cabling (rg6, rg59).
+								</li>
+							</ul>
+						</div>
+						<div className='item-box'>
+							<div style={{fontSize: 30, marginBottom: 40}}>
+								Networking & Testing
+							</div>
+							<ul style={{fontSize: 16}}>
+								<li>Wireless solutions (routers, access points, modems)</li>
+								<li>Patch panels and Switches</li>
+								<li>Extend and terminate network services</li>
+								<li>Network Racks</li>
+								<li>Testing for latency and multi-pair connectivity</li>
+								<li>Providing and installing UPS (Uninterrupted Power Supply)</li>
+							</ul>
 						</div>
 					</div>
 				</section>
 
-				<section id='contact'>
-					<div className='page-content center' style={p4f}>
-						<h2 style={{textAlign: 'center'}}>Contact us</h2>
-						<form>
-							<input className='forminput' placeholder='Your Name' type='text' id='name' name='name'/><br/>
-							<input className='forminput' placeholder='Company Name (optional)' type='text' id='company_name' name='company_name'/><br/>
-							<input className='forminput' placeholder="Referral's Name (optional)" type='text' id='referral' name='referral'/><br/>
-							<input className='forminput' placeholder='Email' type='text' id='email' name='email'/><br/>
-							<input className='forminput' placeholder='Service / Product Requesting' type='text' id='service' name='service'/><br/>
-							<textarea className='forminput large' placeholder='Additional Notes' type='text' id='notes' name='notes'/>
-						</form>
+				<section id='contact us'>
+					<div style={{display: 'flex', width: '90vw', margin: '140px 10vw', justifyContent: 'center'}}>
+						<div style={{display: 'flex', flexDirection: 'column', width: '36vw', margin: 40}}>
+							<h2 style={{width: 600, marginTop: 0, fontSize: 32}}>Book an appointment with us today</h2>
+							<p style={{width: 600, fontSize: 20, margin: 0, marginBottom: 30, color: '#6C9ADA'}}>If you have any questions, feel free to email or give us a call.</p>
+							<form style={{display: 'flex', flexWrap: 'wrap'}}>
+								<div className='form-container'>
+									<label className='formlabel' for="name">Full Name</label><br/>
+									<input className='forminput' type='text' id='name' name='name'/>
+								</div>
+								<div className='form-container'>
+									<label className='formlabel' for="phone">Phone</label><br/>
+									<input className='forminput' type='text' id='phone' name='name'/>
+								</div>
+								<div className='form-container'>
+									<label className='formlabel' for="company_name">Company Name</label><br/>
+									<input className='forminput' type='text' id='company_name' name='company_name'/>
+								</div>
+								<div className='form-container'>
+									<label className='formlabel' for="email">Email Address</label><br/>
+									<input className='forminput' type='text' id='email' name='email'/>
+								</div>
+								<div className='form-container'>
+									<label className='formlabel' for="notes">Additional Notes</label><br/>
+									<textarea className='forminput large' type='text' id='notes' name='notes'/>
+								</div>
+							</form>
+							<div style={{display: 'flex', justifyContent: 'flex-end'}}>
+								<button style={{marginTop: 12, marginRight: 40, width: 200, lineHeight: 1.2}}>Submit</button>
+							</div>
+						</div>
+						<div className='contact-info'>
+							<div style={{margin: 20, fontSize: 20, marginBottom: 40}}>
+								<div style={{display: 'flex', marginBottom: 10, lineHeight: 1.5}}>
+									<img src={emailIco} height={30}/>
+									<div style={{height: 20, width: 2, backgroundColor: 'white', margin: '5px 20px'}}/>
+									Email us
+								</div>
+								ContactUs@initeksolutions.com
+							</div>
+							<div style={{margin: 20, fontSize: 20, marginBottom: 40}}>
+								<div style={{display: 'flex', marginBottom: 10, lineHeight: 1.5}}>
+									<img src={phoneIco} height={30}/>
+									<div style={{height: 20, width: 2, backgroundColor: 'white', margin: '5px 20px'}}></div>
+									Call us
+								</div>
+								(604)-123-1234
+							</div>
+							<div style={{margin: 20, fontSize: 20, marginBottom: 40}}>
+								<div style={{display: 'flex', marginBottom: 10, lineHeight: 1.5}}>
+									<img src={mapsIco} height={30}/>
+									<div style={{height: 20, width: 2, backgroundColor: 'white', margin: '5px 20px'}}></div>
+									Find us
+								</div>
+								123 Fake Street, Vancouver, BC
+							</div>
+						</div>
+
 					</div>
-				</section> */}
+				</section>
 			</main>
 		</>
 	);
